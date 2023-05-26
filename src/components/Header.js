6dd44af91d73c010cd54d0ca541614a9256bb2b5
F F <ktrnstm@gmail.com>
2023-05-26 12:49:12 +0300

    Second commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img src="/img/logo.png" alt="logo" width={40} height={40} />
+        <div className="headerInfo">
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="d-flex">
+        <li className="mr-30">
+          <img src="/img/cart.svg" alt="cart" width={18} height={18} />
+          <span>1205 руб.</span>
+        </li>
+        <li>
+          <img src="/img/user.svg" alt="user" width={18} height={18} />
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
